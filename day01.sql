9ee4f2ce17a59063fba40fb406a983820f74884b
Ahmet Kaya <ahka1143@gmail.com>
2022-06-13 22:49:32 +0300

    first commit

--- day01.sql
+++ day01.sql
@@ -11,3 +11,21 @@ javadaki gibi aciklama yazabiliriz
 
 use batch59;
 -- birden fazla database'miz varsa "use" komutuyla database'i seceriz.
+
+
+
+-- TABLO OLUSTURMA
+create table student
+(
+id varchar(4),
+name varchar(30),
+age int
+
+
+);
+
+
+
+
+
+
