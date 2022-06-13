122ca438d6f122fa2fe29e227bac6260a035bfe1
Ahmet Kaya <ahka1143@gmail.com>
2022-06-13 23:04:40 +0300

    first commit

--- day01.sql
+++ day01.sql
@@ -20,9 +20,27 @@ create table student
 id varchar(4),
 name varchar(30),
 age int
-
-
 );
+
+
+-- VERI GIRISI
+insert into student values('1000','Ali Can','25');
+insert into student values('1001','Veli Yaman','25');
+insert into student values('1002','Ayse Tan','25');
+insert into student values('1003','Derya Duman','25');
+
+select * from student;
+-- student tablosundaki herseyi getirir
+
+select name from student;
+-- student tablusundaki name sutununu getirir
+select id from student;
+-- student tablusundaki id sutununu getirir
+select id, name from student;
+-- student tablusundaki id ve name sutunlarini getirir
+
+
+
 
 
 
