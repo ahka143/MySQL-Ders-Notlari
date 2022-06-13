c49a23bc6f7e0f7d2d5bb041707d2a46ed249619
Ahmet Kaya <ahka1143@gmail.com>
2022-06-13 22:35:39 +0300

    first commit

--- day01.sql
+++ day01.sql
@@ -5,4 +5,9 @@ create database batch59;
 -- aciklama yazabilmek icin  "--" kullanilir.alter
 
 /*
+
+javadaki gibi aciklama yazabiliriz
 */
+
+use batch59;
+-- birden fazla database'miz varsa "use" komutuyla database'i seceriz.
