696e0509d75085a42b871ee656f8cdaf2ef2be26
Ahmet Kaya <ahka1143@gmail.com>
2022-06-15 17:57:16 +0300

    15.06.2022

--- day02.sql
+++ day02.sql
@@ -1,12 +1,12 @@
 use batch59;
 
-create table ogrenciler(id int ; isim varchar(40); adres varchar(100); sinav_notu int);
+create table ogrenciler(id int , isim varchar(40), adres varchar(100), sinav_notu int);
 
-insert into ogrenciler values(120; 'Ali Can';' Ankara'; 75);
-insert into ogrenciler values(121; 'Veli Mert'; 'Trabzon'; 85);
-insert into ogrenciler values(122; 'Ayşe Tan'; 'Bursa'; 65);
-insert into ogrenciler values(123; 'Derya Soylu'; 'Istanbul'; 95);
-insert into ogrenciler values(124; 'Yavuz Bal'; 'Ankara'; 80);
+insert into ogrenciler values('120', 'Ali Can',' Ankara', 75);
+insert into ogrenciler values('121', 'Veli Mert', 'Trabzon', 85);
+insert into ogrenciler values('122', 'Ayşe Tan', 'Bursa', 65);
+insert into ogrenciler values('123', 'Derya Soylu', 'Istanbul', 95);
+insert into ogrenciler values('124', 'Yavuz Bal', 'Ankara', 80);
 
 select * from ogrenciler;
 
@@ -14,7 +14,7 @@ select * from ogrenciler;
 /*
 Verileri SELECT komutu ile veritabanından çekerken filtreleme yapmak için
 SELECT ile birlikte WHERE komutu kullanılır.
-SELECT sutun1; sutun2 ....
+SELECT sutun1, sutun2 ....
 FROM tablo adı WHERE koşul
 */
 -- SORU01: Sınav notu 80'den büyük olan öğrencilerin bilgileri listele
@@ -45,11 +45,13 @@ insert into personel values('1005', 'Yavuz Bal', '80000' );
 insert into personel values('1006', 'Sena Beyaz', '100000' );
 
 select *from personel;
+select maas from personel where isim='Ali Can';
+
 
 -- BETWEEN
 /*
-
-BELIRTTIGIMIZ IKI VERI ARASINDAKI BILGILERI LISTELER
+Belirttiğimiz 2 veri arasindaki bilgileri listeler
+Between de kullandiğimiz değerler de dahildir.
 */
 -- SORU05: id’si 1002 ile 1005 arasında olan personel bilgilerini listele
 
@@ -60,7 +62,7 @@ select * from personel where isim between 'Derya Soylu' and  'Yavuz Bal';
 
 -- SORU07: id'si 1001 , 1002 ve 1004 olan personel bilgilerini listele
 select * from personel where id=1001 or id=1002 or id=1004;
-select * from personel where id in(1001,1002,1004); 
+select * from personel where id in(1001,1002,1004);
 
 
 
