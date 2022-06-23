28a6efa6f9349819fa7fbe8eed98c31faad4a145
Ahmet Kaya <ahka1143@gmail.com>
2022-06-24 00:42:48 +0300

    24.06.2022

---
+++ project1.sql
@@ -0,0 +1,9 @@
+use batch59;
+CREATE TABLE `batch59`.` calisanlar3` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `isim` VARCHAR(45) NULL,
+  `adres` VARCHAR(45) NULL,
+  `mail` VARCHAR(45) NULL,
+  `telefon` CHAR(11) NULL,
+  `ise_baslama_tarihi` DATE NULL,
+  PRIMARY KEY (`id`));
