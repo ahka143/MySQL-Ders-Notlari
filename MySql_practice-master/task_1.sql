45cbb44e21abece85a0ebf08be079d96e8a41225
Ahmet Kaya <ahka1143@gmail.com>
2022-07-02 15:24:49 +0300

    2.7.22

--- MySql_practice-master/task_1.sql
+++ MySql_practice-master/task_1.sql
@@ -1,5 +1,16 @@
+create database MySql_Practice;
+use MySql_practice;
 /* ========================== HASTA PROJESI =================================*/   
-  CREATE TABLE if not exists hastaneler
-   
+  CREATE TABLE if not exists hastaneler(
+  id char(5) primary key,
+  isim char(50),
+  sehir char(20),
+  ozel char 
+  );
+  insert into hastaneler values('H101','ACI MADEM HASTANESI','ISTANBUL','Y');
+  insert into hastaneler values('H102','HASZEKI HASTANESI','ISTANBUL','N');
+  insert into hastaneler values('H103','MEDIKOL HASTANESI','IZMIR','Y');
+  insert into hastaneler values('H104','NEMORYIL HASTANESI','ANKARA','Y');
+  -- drop table hastaneler;
     
  
